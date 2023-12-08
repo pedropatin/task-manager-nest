b09b373fe93b19a2c5cd69f810c98898388406a0
Pedro Patino <pedropatinoaz@gmail.com>
2023-12-07 23:10:22 -0300

    sync

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,10 +8,11 @@ import {
   Patch,
   Query,
 } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -43,8 +44,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ): Task {
+    const { status } = updateTaskStatusDto;
     return this.taskService.updateTaskStatus(id, status);
   }
 }
